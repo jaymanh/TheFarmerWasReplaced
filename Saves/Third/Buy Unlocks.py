e805cde7313bab4f30f87bf1fe0b051a07d05e79
jaymanh <65929600+jaymanh@users.noreply.github.com>
2024-06-27 13:02:14 +1000

    Reorder buys

--- Saves/Third/Buy Unlocks.py
+++ Saves/Third/Buy Unlocks.py
@@ -1,13 +1,13 @@
 def BuyUnlock(Tech = Unlocks.Speed):
 	if not unlock(Tech):
 		cost = get_cost(Tech)
-		Grass(cost)
-		Tree(cost)
-		Carrots(cost)
-		Pumpkin(cost)
-		Maze(cost)
-		Cactus(cost)
 		Dino(cost)
+		Cactus(cost)
+		Maze(cost)
+		Pumpkin(cost)
+		Carrots(cost)
+		Tree(cost)
+		Grass(cost)
 		if Items.Power in cost:
 			amount = cost[Items.Power] * 1.5
 			Power(amount) 
--- Saves/Third/Buy.py
+++ Saves/Third/Buy.py
@@ -3,13 +3,13 @@ def Buy(Item = Items.Carrot_Seed, Amount = 10):
 	if not trade(Item, Amount): 
 		a = False
 		cost = get_cost(Item)
-		Grass(cost, Amount)
-		Tree(cost, Amount)
-		Carrots(cost, Amount)
-		Pumpkin(cost, Amount)
-		Maze(cost, Amount)
-		Cactus(cost, Amount)
 		Dino(cost, Amount)
+		Cactus(cost, Amount)
+		Maze(cost, Amount)
+		Pumpkin(cost, Amount)
+		Carrots(cost, Amount)
+		Tree(cost, Amount)
+		Grass(cost, Amount)
 		Buy(Item, Amount)
 	return a
 		
